f6678d0d6f2f0536921268cad00ae96e88d21ea9
Alex Gascón <alex.gascon1999@gmail.com>
2025-05-27 10:27:46 +0200

    Create data_loader.py

---
+++ utils/data_loader.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from datafc.sofascore import (
+    match_data, match_stats_data, lineups_data, shots_data, momentum_data,
+    goal_networks_data, substitutions_data, standings_data, match_odds_data
+)
+
+def load_match_data(cfg):
+    return match_data(
+        tournament_id=cfg["tournament_id"],
+        season_id=cfg["season_id"],
+        week_number=cfg["week"],
+        data_source=cfg["data_source"],
+        enable_json_export=False,
+        enable_excel_export=False
+    )
+
+def load_lineups(match_df, cfg):
+    return lineups_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
+
+def load_stats(match_df, cfg):
+    return match_stats_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
+
+def load_shots(match_df, cfg):
+    return shots_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
+
+def load_momentum(match_df, cfg):
+    return momentum_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
+
+def load_goal_networks(match_df, cfg):
+    return goal_networks_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
+
+def load_substitutions(match_df, cfg):
+    return substitutions_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
+
+def load_standings(cfg):
+    return standings_data(
+        tournament_id=cfg["tournament_id"],
+        season_id=cfg["season_id"],
+        data_source=cfg["data_source"]
+    )
+
+def load_odds(match_df, cfg):
+    return match_odds_data(
+        match_df=match_df,
+        data_source=cfg["data_source"]
+    )
